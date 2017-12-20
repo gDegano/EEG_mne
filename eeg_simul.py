a2db4c3f3cbab26dcb29d314266a127d54818b34
gdegano <giulio.degano@gmail.com>
2017-12-20 13:00:44 +0100

    First commit!

---
+++ eeg_simul.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Giulio Degano
+PhD Student
+Computational Cognitive Neuroimaging Laboratory
+School of Psychology
+College of Life and Environmental Sciences
+University of Birmingham
+Edgbaston
+Birmingham B15 2TT
+e-mail: gxd606@student.bham.ac.uk
+"""
+
