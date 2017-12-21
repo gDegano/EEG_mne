8517a21a36f288939acdde1cbb144a6fb807fa8e
gdegano <giulio.degano@gmail.com>
2017-12-21 19:53:37 +0100

    Starting with frequency slide

--- eeg_simul.py
+++ eeg_simul.py
@@ -85,13 +85,14 @@ prestim_avg=prestim.average()
 # Now we have a Data_frame.... fun begins
 df=prestim_avg.to_data_frame()
 
+# ARMA modelling
+# needs statsmodels package for python!!
 
 from statsmodels.tsa.arima_model import ARMA
-from statsmodels.tsa.arima_model import ARMAResults
 from pandas import DataFrame
 
 # number of prediction steps
-n_steps=20;
+n_steps=50;
 
 # Extract channel 1
 chan_1=df.values[:,1]
@@ -99,8 +100,13 @@ size = int(len(chan_1) * 0.8)
 train, test = chan_1[0:size], chan_1[size:len(chan_1)]
 
 # Model orders taken from lit...
-model = ARMA(train, order=(18,6))
+model = ARMA(train, order=(22,6))
 model_fit = model.fit(disp=0)
+
+# Plotting of the residuals of the model
+residuals = DataFrame(model_fit.resid)
+residuals.plot(kind='kde')
+plt.show() #should be gaussian-like around zero
 
 fore_chan=model_fit.forecast(steps=n_steps)[0]
 
@@ -108,6 +114,40 @@ plt.plot(fore_chan)
 plt.plot(test[0:n_steps], color='red')
 plt.show()
 
+# Freq sliding
+# from mike cohen code... translated to python
+# https://www.youtube.com/watch?list=PLn0OLiymPak3jjr0hHI9OFXuQyPBQlFdk&v=fqkLgJ0Czoc
+
+import math
+
+# Parameters for filtering
+freq2use = 10
+wavt = np.arange(start=-2, stop=2, step=1/raw.info['sfreq'])
+nData = len(train)
+nKern = len(wavt)
+nConv = nKern+nData-1
+hwave = math.floor((len(wavt)-1)/2)
+
+# init kernel
+s = 8 / (2*np.pi*freq2use)
+val_multi=np.multiply(np.exp(1j*2*np.pi*freq2use*wavt),np.exp(-(np.power(wavt,2))/(2*s*s)))
+# plot kernel
+plt.plot(val_multi)
+plt.show()
+
+#FT
+cmwX = np.fft.fft(val_multi,nConv)
+cmwX = np.divide(cmwX,cmwX.max())
+ft_train = np.fft.ifft(np.multiply(np.fft.fft(train,nConv),cmwX))
+ft_train = ft_train[hwave:-hwave]
+# angle!
+ang_ft_train=np.angle(ft_train)
+
+# some plots
+f, axarr = plt.subplots(2, sharex=True)
+axarr[0].plot(train)
+axarr[1].plot(ang_ft_train)
+plt.show()
 
 
 
